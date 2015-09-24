ed80cbea51a88092e40359152132b2fa0e61b71f
margotduek <margotduek@gmail.com>
2015-09-24 12:51:17 -0500

    :boy: Segundo intento

--- index.js
+++
@@ -1,9 +0,0 @@
-// Get access to our Watson module
-var watson = require('./watson/watson');
-// Set up RESTful resources
-// POST requests to /question are handled by 'watson.question'
-app.post('/question', watson.question);
-
-// Start the http server
-http.createServer(app).listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
